c3d7e956410c1d8365ed4684acb2e29619634450
Kimala Cochran <kimalajoy@gmail.com>
2020-03-19 17:37:36 -0600

    working on round class and test

--- src/Round.js
+++ src/Round.js
@@ -1,7 +1,31 @@
-class Round {
-  constructor() {
+const Turn = require('../src/Turn.js');
 
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.turns = 0;
+    this.guesses = [];
+    this.incorrectGuesses = [];
   }
+
+  returnCurrentCard() {
+    return this.deck.cards[this.turns];
+  }
+
+  takeTurn(playerGuess) {
+    let currentCard = this.returnCurrentCard();
+    let turn = new Turn(playerGuess, currentCard);
+    
+    this.guesses.push(playerGuess);
+
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(currentCard.id);
+    }
+
+    this.turns++;
+    return turn.giveFeedback();
+  }
+
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -6,12 +6,43 @@ const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 
 let round;
+let deck;
+let card1, card2, card3;
 
 describe('Round', ()  => {
+  
+  beforeEach(() => {
+
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    deck = new Deck([card1, card2, card3])
+    round = new Round(deck);
+  });
 
   it('should be a function', () => {
     expect(Round).to.be.a('function');
-  })
+  });
 
+  it('should be an instance of Round', () => {
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it('should return the card currently being played', () => {
+    expect(round.returnCurrentCard()).to.deep.equal(card1);
+  });
+
+  it('should update the turn count', () => {
+    expect(round.turns).to.equal(0);
+
+    round.takeTurn('a');
+    round.takeTurn('b');
+    round.takeTurn('c');
+
+    expect(round.turns).to.equal(3);
+  });
 
 });
